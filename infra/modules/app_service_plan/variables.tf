2f9221b761e171d520137d29bf2f975d1dc1cf19
ramonvdakker <ramon.vdakker@gmail.com>
2025-10-04 18:15:46 +0200

    Updates plan

--- infra/modules/app_service_plan/variables.tf
+++ infra/modules/app_service_plan/variables.tf
@@ -14,9 +14,14 @@ variable "resource_group_name" {
 }
 
 variable "sku_name" {
-  description = "SKU for App Service Plan (e.g., F1 for Free, B1 for Basic)"
+  description = "SKU for App Service Plan (B1+ required for Linux + custom containers)"
   type        = string
-  default     = "F1"
+  default     = "B1"
+
+  validation {
+    condition     = can(regex("^(B[1-3]|S[1-3]|P[1-3]V[2-3]|I[1-3]V[2]|WS[1-3])$", var.sku_name))
+    error_message = "sku_name must be B1 or higher (B1-B3, S1-S3, P1V2-P3V3, etc.) for Linux App Service with custom containers. F1 is not supported."
+  }
 }
 
 variable "tags" {
